7f833d9b1f6a2c13656df77229cb90076182dcf7
Damoiskii <moimyazz@gmail.com>
2025-03-11 18:36:20 -0500

    Initial commit

---
+++ src/test/java/com/moiskii/lmsserver/LmsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moiskii.lmsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
